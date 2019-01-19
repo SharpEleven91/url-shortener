82b175ae012b08fdf7281b60d10329fd387dc87a
Cody Biller <sharpeleven91@gmail.com>
2019-01-19 18:52:19 -0500

    fix packages

--- routes/shortenUrl.js
+++ routes/shortenUrl.js
@@ -2,9 +2,8 @@ const validUrl = require("valid-url");
 const shortid = require("shortid");
 const mongoose = require("mongoose");
 const ShortUrl = mongoose.model("ShortUrl");
-const path = require('path');
+const path = require("path");
 module.exports = function(app) {
-
   // route redirects to the original URL given a short url
   app.get("/:shortId", async (req, res) => {
     const hashedUrl = req.params.shortId; // extract the hashed shortID from the request
@@ -13,18 +12,19 @@ module.exports = function(app) {
       return res.redirect(query.originalUrl); // redirect the user to the hasedUrls original URL
     else res.status(404).json(err); // else throw error
   });
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
   // create new hash URL and place in database
   app.post("/api/url-shortener", async (req, res) => {
     let { originalUrl, baseUrl } = req.body; // extract originalUrl and baseUrl from request body
     originalUrl = originalUrl.toLowerCase(); // normalize originalUrls so duplicates are not created in database because of case-sensitivity when search
-    if (!validUrl.isUri(baseUrl) || originalUrl === "") { // if the baseUrl is invalid throw an error
+    if (!validUrl.isUri(baseUrl) || originalUrl === "") {
+      // if the baseUrl is invalid throw an error
       return res.status(401).json("Bad Base URL");
     }
-    const urlID = shortid.generate(); // generate hash id 
+    const urlID = shortid.generate(); // generate hash id
     const updatedAt = new Date(); // create new date
     if (validUrl.isUri(originalUrl)) {
       try {
@@ -33,7 +33,8 @@ module.exports = function(app) {
           return res.status(200).json(query); // if original has already been shortened and is within the database return it
         } else {
           const shortUrl = baseUrl + "/" + urlID; // construct short url format
-          let newEntry = new ShortUrl({  // creates a new entry conforming to ShortUrl schema
+          let newEntry = new ShortUrl({
+            // creates a new entry conforming to ShortUrl schema
             originalUrl,
             shortUrl,
             urlID,
@@ -43,7 +44,7 @@ module.exports = function(app) {
           res.status(200).json(newEntry); // return new entry
         }
       } catch (err) {
-        res.status(401).json(err); // throw error if anything fails 
+        res.status(401).json(err); // throw error if anything fails
       }
     } else {
       res.status(401).json("bad url");
--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 // module imports
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const database = require('./config/db');
+const database = require("./config/db");
 const PORT = 8000;
-const path = require('path');
+const path = require("path");
 const connectionOps = {
-    keepAlive: true, 
-    reconnectTries: Number.MAX_VALUE,
-    useNewUrlParser: true
-}
-
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useNewUrlParser: true
+};
 
 mongoose.Promise = global.Promise;
 mongoose.connect(database.url, connectionOps, (err, db) => {
-    if (err) return console.log(`Error: ${err}`);
-    console.log('Database connected');
-    app.use(bodyParser.json());
-    require('./models/ShortUrl');
-    app.use(express.static(path.join(__dirname + '/client/build')));
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
-    app.listen(PORT, () => {
-        console.log(`Listening on port: ${PORT}`)
-        require('./routes/shortenUrl')(app);
-    });
+  if (err) return console.log(`Error: ${err}`);
+  console.log("Database connected");
+  app.use(bodyParser.json());
+  require("./models/ShortUrl");
+  app.use(express.static(path.join(__dirname + "/client/build")));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+    require("./routes/shortenUrl")(app);
+  });
 });
-
