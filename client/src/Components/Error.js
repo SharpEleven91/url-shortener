8cadf151495321985fb9d1def600eafca28fbfa7
Cody Biller <sharpeleven91@gmail.com>
2019-01-21 18:19:04 -0500

    refactor Error component to functional component

--- client/src/Components/Error.js
+++ client/src/Components/Error.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Fade from "@material-ui/core/Fade";
-class Error extends Component {
-  render() {
+function Error(props) {
     return (
-      <Fade in={this.props.checked} timeout={{ enter: 1700, exit: 1700 }}>
+      <Fade in={props.checked} timeout={{ enter: 1700, exit: 1700 }}>
         <Typography color="error" component="h3" variant="h3" gutterBottom>
           {" "}
           Invalid URL{" "}
         </Typography>
       </Fade>
     );
-  }
 }
 
 export default Error;
