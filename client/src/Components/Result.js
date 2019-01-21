517f0b18e3133c8269c57f700937c30c8271063e
Cody Biller <sharpeleven91@gmail.com>
2019-01-21 18:17:50 -0500

    rework copy logic and remove react-clipboard

--- client/src/Components/Result.js
+++ client/src/Components/Result.js
@@ -1,38 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Typography from "@material-ui/core/Typography";
 import Fade from "@material-ui/core/Fade";
 import IconButton from "@material-ui/core/IconButton";
 import { Replay } from "@material-ui/icons";
 import { CheckCircle } from "@material-ui/icons";
 import { Assignment } from "@material-ui/icons";
-import Clipboard from "react-clipboard.js";
 
 export default function Result(props) {
   const [copySuccess, setCopySuccess] = useState(false);
-
+  const textRef = useRef(props.result);
+  const TextResult = () => <input id="hidden" ref={textRef} value={props.result} disabled></input>
+  const handleChange = () => {
+    if (document.execCommand) {
+      textRef.current.select()
+      document.execCommand("copy");
+    }
+    setCopySuccess(true);
+  }
     return (
       <Fade in={true}>
         <div>
-          <Typography component="h4" variant="h4" gutterBottom>
-            {" "}
-            {props.result}{" "}
+          <TextResult/>
+          <Typography variant="h4" gutterBottom>
+            {props.result}
           </Typography>
           <IconButton onClick={props.reset}>
             <Replay fontSize="large" />
           </IconButton>
-          <Clipboard
-            style={{ border: "none", background: "none" }}
-            data-clipboard-text={props.result}
-            onSuccess={setCopySuccess(true)}
-          >
-            <IconButton>
+            <IconButton onClick={handleChange}>
               {copySuccess ? (
                 <CheckCircle fontSize="large" />
               ) : (
                 <Assignment fontSize="large" />
               )}
             </IconButton>
-          </Clipboard>
         </div>
       </Fade>
     );
--- client/src/tests/Error.react.test.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import Error from '../Components/Error';
-import renderer from 'react-test-renderer';
