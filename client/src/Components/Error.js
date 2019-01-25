b2232675976e47978e7f64fdaaeef2b1f4a6e588
Cody Biller <sharpeleven91@gmail.com>
2019-01-24 20:36:51 -0500

    update error component test

--- client/src/Components/Error.js
+++ client/src/Components/Error.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Typography, Fade } from "@material-ui/core/Typography";
-const Error = (props) => {
+import { Typography, Fade } from "@material-ui/core";
+
+export default function Error(props) {
   return (
     <Fade in={props.checked} timeout={{ enter: 1700, exit: 1700 }}>
-      <Typography color="error" component="h3" variant="h3" gutterBottom>
+      <Typography
+        data-testid="error-display"
+        color="error"
+        component="h3"
+        variant="h3"
+        gutterBottom
+      >
         {" "}
         Invalid URL{" "}
       </Typography>
     </Fade>
   );
 }
-
-export default Error;
--- client/src/tests/Error.test.js
+++ client/src/tests/Error.test.js
@@ -1,11 +1,10 @@
-import { mount, configure } from 'enzyme';
 import React from 'react';
 import expect from 'expect';
 import Error from '../Components/Error';
 import { render } from 'react-testing-library';
+import "jest-dom/extend-expect";
 
-test('Component displays error properly', () => {
-    const { container } = render(<Error/>);
-    const text = container.firstChild;
-    expect(text.textContent).toBe(" Invalid URL ");
+test('Component renders properly', () => {
+    const { queryByTestId, container } = render(<Error checked={true}/>);
+    expect(queryByTestId("error-display")).toHaveTextContent("Invalid URL");
 })
